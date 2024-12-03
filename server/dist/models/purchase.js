13effa201dd688d8b890c41a2271fe440d802a2a
BrayanJosePerezRamos <alu0101431185@ull.edu.es>
2024-12-03 12:01:02 +0000

    borrar dist

--- server/dist/models/purchase.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-const purchaseSchema = new Schema({
-    user: { type: String, required: true },
-    total_price: { type: Number, required: true },
-    date: { type: String, required: true }
-});
-export const Purchase = model("Purchase", purchaseSchema);
-export default Purchase;
